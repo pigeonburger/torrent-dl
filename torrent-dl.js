0975ae4aa5d63309f59601fd1506bf4af5834b45
Pigeonburger <pigeonburger@hotmail.com>
2021-09-08 20:20:57 +1000

    Unleash the beast 🥶

--- torrent-dl.js
+++ torrent-dl.js
@@ -7,9 +7,9 @@ var progress = require("progress");
 var colors = require("colors");
 var parsetorrent = require("parse-torrent");
 var torrentStream = require("torrent-stream");
-const { exit } = require('yargs');
 
 const argv = yargs
+.usage("Torrent-DL\nCommand-Line Torrent Downloader\n\nUsage: torrent-dl -i [MAGNET LINK OR TORRENT FILE] [OPTIONS]")
 .option('input', {
     alias: 'i',
     description: 'Magnet link, torrent file or link to torrent file.',
@@ -18,29 +18,46 @@ const argv = yargs
 })
 .option('port', {
     alias: 'p',
-    description: 'Port to listen on (default 6881)',
+    description: 'Port to listen on.',
     type: 'number',
     default: 6881,
 })
 .option('connections', {
     alias: 'c',
-    description: 'Max number of peers to connect to (default 100)',
+    description: 'Max number of peers to connect to.',
+    type: 'number',
     default: 100
     
 })
 .option('uploadslots', {
     alias: 'u',
-    description: 'Number of upload slots (default 10)',
+    description: 'Number of upload slots.',
+    type: 'number',
     default: 10,
 })
+.option('tracker', {
+    alias: 't',
+    description: 'Manually add a tracker URL (can be used more than once if adding multiple trackers)',
+    type: 'string',
+    default: null,
+})
+.option('verbose', {
+    alias: 'v',
+    description: 'Print extra informational output.',
+    type: 'boolean',
+    default: false,
+})
 .help()
+.version("Torrent-DL Version 1.0.0")
 .alias('help', 'h')
 .argv;
 
-var input = argv.input;
+var input = argv.input
+console.log(input)
 var port = argv.port;
-var maxpeers = argv.peers;
+var maxpeers = argv.connections;
 var upslots = argv.uploadslots;
+var trackers = argv.tracker;
 
 var totalLength = null;
 var downLength = null;
@@ -56,10 +73,18 @@ var bytes = function(num) {
 };
 
 var setupEngine = function(torrent) {
+    console.log("Fetching files.....");
+    if (argv.verbose){
+        console.log("Downloading from torrent: " + input)
+        console.log("Listening on port " + port)
+        console.log("Maximum peer connections: " + maxpeers)
+        console.log("Upload slots: " + upslots)
+    }
   engine = torrentStream(torrent, {
     connections: maxpeers,
     uploads: upslots,
     path: ".",
+    trackers: trackers
   });
 
   engine.listen(port);
@@ -71,9 +96,13 @@ var setupEngine = function(torrent) {
   });
 
   engine.on("ready", function() {
+    console.log("\n------------------");
+    console.log(colors.bold.magenta("Found " + Object.keys(engine.files).length + " files:"));
     engine.files.forEach(function(file) {
-      file.select();
+        console.log(colors.yellow(file.name) + " | " + colors.green(bytes(file.length)));
+        file.select();
     });
+    console.log("------------------\n");
 
     totalLengthBytes = engine.files.reduce(function(prevLength, currFile) {
       return prevLength + currFile.length;
@@ -85,33 +114,23 @@ var setupEngine = function(torrent) {
 
     bar = new progress(
       "Downloading " +
-        engine.files.length + " files (:perc/" + bytes(totalLengthBytes) + ") [:bar] :percent :etas :speed :peers",
+        engine.files.length + " files (:perc/" + bytes(totalLengthBytes) + ") |:bar| :percent ETA: :etas :speed :peers",
       {
-        complete: "=",
+        complete: "█",
         incomplete: " ",
         width: 30,
         total: totalLength
       }
     );
 
-    timerId = setInterval(draw, 500);
+    timerId = setInterval(draw, 200);
   });
 
   engine.on("idle", function() {
     engine.destroy();
     clearInterval(timerId);
     clivas.clear();
-    console.log("------------------");
-    engine.files.forEach(function(file) {
-      console.log(file.name + " " + bytes(file.length));
-    });
-    console.log("------------------");
-    console.log(
-      " downloaded " +
-        engine.files.length +
-        " files (" +
-        bytes(totalLength) +
-        ")"
+    console.log("\nDownloaded " + engine.files.length + " files (" + bytes(totalLengthBytes) + ")"
     );
   });
 };
@@ -132,8 +151,35 @@ var draw = function() {
 
   if (verified >= engine.torrent.pieces.length) {
     clivas.clear();
-    clivas.line(" downloading last few pieces " + speed + " " + peers);
+    clivas.line("Downloading last few pieces " + speed + " " + peers);
   } else {
+    if (argv.verbose){
+      console.clear();
+      console.log("\n------------------");
+    console.log(colors.bold.magenta("Found " + Object.keys(engine.files).length + " files:"));
+    engine.files.forEach(function(file) {
+        console.log(colors.yellow(file.name) + " | " + colors.green(bytes(file.length)));
+        file.select();
+    });
+    console.log("------------------\n");
+    var peertable = [];
+    console.log("Downloading from torrent: " + input);
+    console.log("Listening on port " + port);
+    console.log("Maximum peer connections: " + maxpeers);
+    console.log("Upload slots: " + upslots);
+    console.log("\nPEER INFO:");
+    for (let p of engine.swarm.wires){
+        let vals = p['peerAddress'].split(":");
+        let ip = vals[0];
+        let pport = parseInt(vals[1], 10);
+        let downloaded = p['downloaded'];
+        let uploaded = p['uploaded']
+        let data = {IP: ip, Port: pport, Downloaded: bytes(downloaded), Uploaded: bytes(uploaded)};
+        peertable.push(data);
+    }
+    console.table(peertable);
+    console.log('\n');
+}
     bar.tick(verified - prevLength, {
       speed: _speed.green,
       peers: peers,
